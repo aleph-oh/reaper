fd2cc0b66b52727e45301e9cf929edf08b5ee2b4
aleph-oh <jhilton@mit.edu>
2023-11-17 16:24:06 -0500

    Add table to field

--- reaper-lib/src/types.rs
+++ reaper-lib/src/types.rs
@@ -1,28 +1,55 @@
 #[derive(Debug, Eq, PartialEq, Clone)]
-pub struct Field {name: String}
+pub struct Field {
+    name: String,
+    table: String,
+}
 type Fields = Vec<Field>;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum ASTNode {
-  Select {fields: Option<Fields>, table: Box<ASTNode>, pred: PredNode}, 
-  Join {table1: Box<ASTNode>, table2: Box<ASTNode>, pred: PredNode}, // Note: Rel, Rel can be expressed as select _ from _, _ where True
-  Table {name: String},
-  Field {name: String}, 
+    Select {
+        fields: Option<Fields>,
+        table: Box<ASTNode>,
+        pred: PredNode,
+    },
+    Join {
+        table1: Box<ASTNode>,
+        table2: Box<ASTNode>,
+        pred: PredNode,
+    }, // Note: Rel, Rel can be expressed as select _ from _, _ where True
+    Table {
+        name: String,
+    },
+    Field {
+        name: String,
+    },
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum ExprNode {
-  FieldName {name: String}, 
-  Int {value: isize},
+    FieldName { name: String },
+    Int { value: isize },
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum PredNode {
-  Lt {left: ExprNode, right: ExprNode},
-  Eq {left: ExprNode, right: ExprNode},
-  And {left: Box<PredNode>, right: Box<PredNode>},
+    Lt {
+        left: ExprNode,
+        right: ExprNode,
+    },
+    Eq {
+        left: ExprNode,
+        right: ExprNode,
+    },
+    And {
+        left: Box<PredNode>,
+        right: Box<PredNode>,
+    },
 }
 
-pub struct ConcTable {columns: Vec<String>, values: Vec<Vec<isize>>}
+pub struct ConcTable {
+    columns: Vec<String>,
+    values: Vec<Vec<isize>>,
+}
 
 pub type Examples<'a, 'b> = &'a [&'b [(ConcTable, ConcTable)]];
