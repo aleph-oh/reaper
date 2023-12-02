3f8ffc298e7bf001a9153268dc6ae5dae08421db
aleph-oh <jhilton@mit.edu>
2023-12-02 18:44:50 -0500

    predicate synthesis: implement more TODOs

--- reaper-lib/src/stun.rs
+++ reaper-lib/src/stun.rs
@@ -35,6 +35,8 @@ pub fn synthesize_pred<'a>(
     query: &ASTNode,
     target: &ConcTable,
     conn: &rusqlite::Connection,
+    fields: &[String],
+    constants: &[isize],
     max_depth: usize,
 ) -> Result<Vec<PredNode>, SynthesisError> {
     let query = match AbstractQuery::try_from(query) {
@@ -42,7 +44,7 @@ pub fn synthesize_pred<'a>(
         Err(InvalidQueryError::TooFewPredicates) => return Ok(vec![PredNode::True]),
         Err(e) => return Err(e)?,
     };
-    let mut predicates = synthesize(&query, target, conn, max_depth)?;
+    let mut predicates = synthesize(&query, target, conn, fields, constants, max_depth)?;
     predicates.sort_unstable_by_key(PredNode::height);
     Ok(predicates)
 }
@@ -136,7 +138,12 @@ impl AbstractQuery {
                 table: Rc::clone(table),
                 pred,
             },
-            ASTNode::Join { fields, table1, table2, .. } => ASTNode::Join {
+            ASTNode::Join {
+                fields,
+                table1,
+                table2,
+                ..
+            } => ASTNode::Join {
                 fields: fields.clone(),
                 table1: Rc::clone(table1),
                 table2: Rc::clone(table2),
@@ -170,32 +177,98 @@ impl TryFrom<&ASTNode> for AbstractQuery {
 
 impl ConcTable {
     fn to_intermediate<'a>(&self, base: &'a ConcTable) -> IntermediateTable<'a> {
-        todo!()
+        IntermediateTable {
+            base,
+            rows: bv::bitvec![1; base.values.len()],
+        }
     }
 }
 
-fn predicates_with_depth(depth: usize) -> Vec<PredNode> {
-    todo!()
+// TODO: maybe not strings here?
+struct Environment(HashMap<String, isize>);
+
+impl Environment {
+    fn from_row(table: &ConcTable, i: usize) -> Self {
+        let names = &table.columns;
+        let v = &table.values[i];
+        let map: HashMap<_, _> = names
+            .iter()
+            .map(|name| format!("{}.{}", table.name, name))
+            .zip(v.iter().copied())
+            .collect();
+        Self(map)
+    }
+}
+
+impl ExprNode {
+    fn eval(&self, env: &Environment) -> isize {
+        match self {
+            ExprNode::FieldName { name } => *env
+                .0
+                .get(name)
+                .expect("well-formed predicate implies a value in env"),
+            ExprNode::Int { value } => *value,
+        }
+    }
 }
 
 impl PredNode {
-    fn eval(&self, table: &ConcTable, i: usize) -> bool {
-        todo!()
+    fn eval(&self, env: &Environment) -> bool {
+        match self {
+            PredNode::True => true,
+            PredNode::Lt { left, right } => left.eval(env) < right.eval(env),
+            PredNode::Eq { left, right } => left.eval(env) == right.eval(env),
+            PredNode::And { left, right } => left.eval(env) && right.eval(env),
+        }
     }
 }
 
-fn grow<'a>(with: &[PredNode]) -> Vec<PredNode> {
-    todo!()
+fn base_exprs(fields: &[String], constants: &[isize]) -> Vec<ExprNode> {
+    fields
+        .iter()
+        .map(|name| ExprNode::FieldName {
+            name: name.to_string(),
+        })
+        .chain(constants.iter().map(|n| ExprNode::Int { value: *n }))
+        .collect()
 }
 
-fn elim(v: &mut Vec<PredNode>) {
-    todo!()
+fn base_preds(fields: &[String], constants: &[isize]) -> Vec<PredNode> {
+    let exprs = base_exprs(fields, constants);
+    let mut new = Vec::with_capacity(exprs.len() * 2);
+    new.push(PredNode::True);
+    for (i, left) in exprs.iter().enumerate() {
+        for right in exprs[i..].iter() {
+            new.push(PredNode::Eq {
+                left: left.clone(),
+                right: right.clone(),
+            });
+            new.push(PredNode::Lt {
+                left: left.clone(),
+                right: right.clone(),
+            });
+        }
+    }
+    new
+}
+
+fn grow(with: &[PredNode], base_predicates: &[PredNode]) -> Vec<PredNode> {
+    let mut new = Vec::with_capacity(with.len());
+    for p1 in with.iter().chain(base_predicates.iter()) {
+        new.push(PredNode::And {
+            left: Box::new(p1.clone()),
+            right: Box::new(PredNode::True),
+        });
+    }
+    new
 }
 
 fn synthesize(
     query: &AbstractQuery,
     target: &ConcTable,
     conn: &rusqlite::Connection,
+    fields: &[String],
+    constants: &[isize],
     max_depth: usize,
 ) -> Result<Vec<PredNode>, SynthesisError> {
     // The Scythe paper implements predicate search as a top-down search where we try to generate predicates (simplest-first)
@@ -229,6 +302,9 @@ fn synthesize(
     // Now, phrase the concrete table as a bitvector.
     let target_intermediate = target.to_intermediate(&rows);
 
+    // TODO: how do we get the space of all fields? I'm assuming it can be passed in as a parameter here, but
+    // I think it might depend on our abstract query?
+    let base_predicates = base_preds(fields, constants);
     let mut prior_depth_predicates = vec![PredNode::True];
     let mut vec_to_preds = HashMap::new();
     vec_to_preds.insert(bv::bitvec![1; rows.values.len()], vec![PredNode::True]);
@@ -236,19 +312,19 @@ fn synthesize(
         use itertools::Itertools;
         // NOTE: this form of predicate generation is maybe a little silly. What if we tried to find
         // predicates that, when AND-ed, make the right thing (for example)?
-        let mut predicates = grow(&prior_depth_predicates);
-        elim(&mut predicates);
 
-        let mut new_predicates: HashMap<_, _> = predicates
+        // We don't do elimination here because it'll happen when we construct the new predicates anyways.
+        let mut predicates: HashMap<_, _> = grow(&prior_depth_predicates, &base_predicates)
             .into_iter()
             .map(|pred| {
                 let mut v = bv::bitvec![0; rows.values.len()];
                 // TODO: here, make it possible to compute the new bitvectors w/o doing this computation
                 // by doing bitwise ops. Right now we have to re-evaluate each time by testing the predicate,
                 // which is pretty slow.
-                v.iter_mut()
-                    .enumerate()
-                    .for_each(|(i, mut x)| *x = pred.eval(&rows, i));
+                v.iter_mut().enumerate().for_each(|(i, mut x)| {
+                    let env = Environment::from_row(&rows, i);
+                    *x = pred.eval(&env)
+                });
                 (pred, v)
             })
             // v must be a superset of the target rows since we don't have disjunction.
@@ -266,15 +342,15 @@ fn synthesize(
             .collect();
 
         // When we find a predicate that has the right rows, stop and return it.
-        if let Some((rep, mut rest)) = new_predicates.remove(&target_intermediate.rows) {
+        if let Some((rep, mut rest)) = predicates.remove(&target_intermediate.rows) {
             rest.push(rep);
             return Ok(rest);
         }
 
         // Lastly, if we haven't found matches at this depth, go to the next depth,
         // which can build on these predicates.
-        let mut new_prior_depth_predicates = Vec::with_capacity(new_predicates.iter().len());
-        for (v, (rep, rest)) in new_predicates.into_iter() {
+        let mut new_prior_depth_predicates = Vec::with_capacity(predicates.iter().len());
+        for (v, (rep, rest)) in predicates.into_iter() {
             new_prior_depth_predicates.push(rep.clone());
             let e = vec_to_preds
                 .entry(v)
@@ -285,5 +361,8 @@ fn synthesize(
 
         prior_depth_predicates = new_prior_depth_predicates;
     }
+
+    // Finding nothing doesn't indicate an error, but it does indicate that there might be no
+    // solutions, or the user will have to try another depth.
     Ok(vec![])
 }
