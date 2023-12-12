145721d3177ec536e40f638f2df05a2582bef0db
aleph-oh <jhilton@mit.edu>
2023-12-12 04:59:23 -0500

    add integration to library toplevel

--- reaper-lib/src/lib.rs
+++ reaper-lib/src/lib.rs
@@ -1,3 +1,8 @@
+use std::collections::{HashMap, HashSet};
+
+use bitvec::prelude as bv;
+use enum_predicates::enum_and_group_predicates;
+use sql::eval_abstract;
 use thiserror::Error;
 
 pub mod bottomup;
@@ -14,10 +19,64 @@ fn query_rank(_q: &types::AST<types::PredNode>) -> isize {
 
 #[derive(Error, Debug)]
 pub enum SynthesisError {
-    #[error("failed to synthesize predicate")]
-    Predicate(#[from] stun::PredicateSynthesisError),
+    #[error("failed to enumerate predicates")]
+    Enumeration(#[from] enum_predicates::PredicateEnumerationError),
+    #[error("failed during BVDFS to find target bitvector")]
+    BVDFS(#[from] bvdfs::BVDFSError),
+    #[error("failed during database query")]
+    Database(#[from] rusqlite::Error),
     #[error("failed to find a satisfying query")]
     NoQueriesFound,
+}
+
+impl types::ConcTable {
+    fn subset_bitvec(&self, other: &Self) -> bv::BitVec {
+        let other_rows = other.values.iter().collect::<HashSet<_>>();
+        let mut v = bv::bitvec![0; self.values.len()];
+        for (mut b, row) in v.iter_mut().zip(self.values.iter()) {
+            *b = other_rows.contains(row);
+        }
+        v
+    }
+}
+
+pub fn synthesize(
+    q: &types::AST<()>,
+    target: &types::ConcTable,
+    constants: &[isize],
+    fields: &[types::Field],
+    max_predicate_depth: usize,
+    conn: &rusqlite::Connection,
+) -> Result<Vec<types::AST<types::PredNode>>, SynthesisError> {
+    let predicates = enum_and_group_predicates(q, constants, fields, max_predicate_depth, conn)?;
+    let representatives: Vec<_> = predicates
+        .values()
+        .map(|v| {
+            v.first()
+                .expect("each vec in predicates must not be empty!")
+        })
+        .cloned()
+        .collect();
+    let bitvectors = bvdfs::bvdfs(q, &representatives, &mut HashMap::new(), conn)?;
+    // TODO: make the return type of bvdfs less stupid. probably should be a hashmap from bitvecs to all predicate vectors that
+    // produce that value.
+    let t = eval_abstract(q, conn)?;
+    let target_bv = t.subset_bitvec(target);
+    let queries = bitvectors
+        .into_iter()
+        .filter_map(|(bv, preds)| {
+            if bv == target_bv {
+                // TODO: with_predicates should probably accept an im::Vector instead.
+                let q = q
+                    .with_predicates(&preds.into_iter().collect::<Vec<_>>())
+                    .expect("query substitution failed!");
+                Some(q)
+            } else {
+                None
+            }
+        })
+        .collect();
+    Ok(queries)
 }
 
 #[cfg(test)]
