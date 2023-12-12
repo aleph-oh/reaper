dd8dcb43baaba766e2d5ebfa68c31b72ec6e6e48
aleph-oh <jhilton@mit.edu>
2023-12-12 05:12:05 -0500

    run clippy on reaper binary

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use reaper_lib::sql::create_table;
 use reaper_lib::types::*;
 use rocket::fs::{relative, FileServer};
 use rocket::serde::json::Json;
-use rocket::serde::{Deserialize, Serialize};
+use rocket::serde::{Deserialize};
 
 #[derive(Deserialize)]
 struct Example {
@@ -26,7 +26,7 @@ fn synth(example: Json<Example>) {
             depth,
             &conn,
         );
-        println!("{}", "looking for predicate...");
+        println!("looking for predicate...");
         for query in queries.iter() {
             let predicate = reaper_lib::synthesize(
                 query,
