04ee088e797a80c6aaccbc83be4f2a09c4d76f73
Katherine Mohr <kmohr@mit.edu>
2023-12-12 12:09:49 -0500

    have the simplest thing working

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rocket;
 extern crate serde;
 
 use reaper_lib::bottomup::{generate_abstract_queries, get_fields};
-use reaper_lib::sql::create_table;
+use reaper_lib::sql::{create_table, create_sql_query};
 use reaper_lib::types::*;
 use rocket::fs::{relative, FileServer};
 use rocket::serde::json::Json;
@@ -17,9 +17,9 @@ struct Example {
 }
 
 #[post("/synth", format = "json", data = "<example>")]
-fn synth(example: Json<Example>) {
+fn synth(example: Json<Example>) -> String {
     let conn = create_table(&example.input).unwrap();
-    for depth in 1..=2 {
+    for depth in 1..=3 {
         println!("Depth: {}", depth);
         let queries = generate_abstract_queries(
             (example.input.clone(), example.output.clone()),
@@ -32,12 +32,16 @@ fn synth(example: Json<Example>) {
                 reaper_lib::synthesize(query, &example.output, &example.constants, 3, &conn)
                     .map(|ps| ps.first().expect("vec must not be empty").clone());
             println!("Predicate: {:?}", predicate);
-            if predicate.is_ok() {
-                println!("Found predicate: {:?}", predicate);
-                return;
+            if let Ok(predicate) = predicate {
+                let sql = create_sql_query(&predicate);
+                // Remove beginning and ending parens
+                let sql = &sql[1..sql.len() - 1];
+                println!("SQL: {}", sql);
+                return sql.to_string()
             }
         }
     }
+    "Unable to synthesize".to_string()
 }
 
 #[launch]
