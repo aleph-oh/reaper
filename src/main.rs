269c3a8e47c36016334f2d6d2c210ac08c229fff
Katherine Mohr <kmohr@mit.edu>
2023-12-11 23:50:46 -0500

    woot woot UI

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 #[macro_use]
 extern crate rocket;
+extern crate serde;
 
 use rocket::fs::{relative, FileServer};
 use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+use reaper_lib::types::*;
 
-struct Example<'r> {
-  name: &'r str,
+#[derive(Deserialize)]
+struct Example {
+    input: Vec<ConcTable>,
+    output: ConcTable,
+    constants: Vec<i32>,
 }
 
 #[post("/synth", format = "json", data = "<example>")]
-fn new_user(user: Json<Example>) {
-    /* ... */
+fn new_user(example: Json<Example>) {
+    println!("Received example: {:?}", example.constants);
+}
+
+#[get("/synth2")]
+fn test() {
+    println!("{}", "hi jay");
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", FileServer::from(relative!("/static")))
+        .mount("/", routes![new_user, test])
 }
